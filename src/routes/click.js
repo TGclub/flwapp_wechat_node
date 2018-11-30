c51dea0c64c94947ea4fbf2dd2c9e9ec3f8c20be
ACERY1 <y841034081@gmail.com>
2018-11-30 09:00:08 +0800

    feat:添加业务路由

--- src/routes/click.js
+++ src/routes/click.js
@@ -3,39 +3,31 @@
  * /click/*
  */
 import wechat from 'wechat';
-import wechatAPI from 'wechat-api';
 import xml2json from 'xml2json';
+import wechatConfig from '../keys/wechatAccount.json';
 const router = require('express').Router();
 
 
 /**
- * /click
+ * /click/donate
  * 点击”捐赠“后请求的路由
  * 获取 openid -> 构造URL -> 返回URL
  */
-router.get('/', (req, res) => {
-  // res.logEmail.error('hello world');
-  // res.wxAPI.uploadLogo('filepath', (err) => {
-  //   res.logError.error(err)
-  // } );
-
-  res.write('done');
-  res.end();
-});
-
-router.post('/test', (req, res) => {
-  req.rawBody = '';
+router.post('/', wechat(wechatConfig, (req, res) => {
+  console.log(req.weixin);
+  let rawBody = '';
   req.setEncoding('utf8');
   req.on('data', (chunk) => {
-    req.rawBody += chunk;
+    rawBody += chunk;
   });
 
   req.on('end', () => {
-    const json = xml2json.toJson(req.rawBody);
+    const data = xml2json.toJson(rawBody);
+    // eslint-disable-next-line
     // {"xml":{"ToUserName":"toUser","FromUserName":"FromUser","CreateTime":"123456789","MsgType":"event","Event":"CLICK","EventKey":"EVENTKEY"}}
-    res.send(json);
+    console.log(data);
     res.end();
   });
-});
+}));
 
 export default router;
